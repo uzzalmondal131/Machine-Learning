f854d7737d5ba8b8743d1e3919d2d682355e2b82
uzzalmondal131 <uzzalmondal131@gmail.com>
2020-02-01 22:51:42 +0530

    Simple Linear Regression

---
+++ Simple-Linear-Regression/simple_linear_regression.py
@@ -0,0 +1,11 @@
+# Simple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+dataset.head(5)
+
